15c4f50a22950373ae504f967eda083fc0ad8aae
João Victor de Carvalho <joao52304@outlook.com.br>
2025-08-19 12:43:54 -0300

    Primeiro Commit

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Xml.Schema;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -17,14 +19,14 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentOutOfRangeException("O numero de hospedes deve ser maior ou igual a capcacidade da suite");
             }
         }
 
@@ -36,25 +38,31 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            int total = 0;
+            foreach (var hospedes in Hospedes)
+            {
+                total =+ Hospedes.Count;
+                
+            }
+            return total;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            
+            decimal valor = Suite.ValorDiaria;
+            decimal valorTotalDiaria = valor * DiasReservados;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valorTotalDiaria = (valor * DiasReservados) * 10/100;
             }
 
-            return valor;
+            return valorTotalDiaria;
         }
     }
 }
